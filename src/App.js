65ad92c05a81b6a697778d011ad7a400ba1f13a9
ugur.ayaz <ugur.ayaz@metus.com>
2022-08-13 20:35:22 +0300

    added page note found

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import Contacts from './components/Contacts'
 import { Routes, Route } from 'react-router-dom'
 import Edit from './components/Contacts/Edit'
+import Error404 from './components/Error404'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Contacts />} />
                     <Route path="/edit/:id" element={<Edit />} />
+                    <Route path="*" element={<Error404 />} />
                 </Routes>
             </div>
         </div>
